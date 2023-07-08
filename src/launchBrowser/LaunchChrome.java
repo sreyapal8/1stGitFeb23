56ea3e6b1bf8dc91d69ff9b6ab6bf8e2fec3c50a
sreyapal8 <sreyapal8@gmail.com>
2023-07-08 12:02:26 -0500

    committed

---
+++ src/launchBrowser/LaunchChrome.java
@@ -0,0 +1,21 @@
+package launchBrowser;
+
+import java.util.Scanner;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		WebDriver driver=new ChromeDriver();
+		
+		
+		
+		
+		
+	}
+
+}
